8b26aed11454694b882254ec407084b29b2f47a0
Fuadmikayil <mikayilov2006@gmail.com>
2025-12-27 21:46:40 +0400

    png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,9 @@ export const metadata: Metadata = {
   description:
     "Rəşidoğlu İnşaat MMC müştərilərinə bir ünvandan tam və etibarlı tikinti məhsulları təqdim edir. Construction company website",
   icons: {
-    icon: "/images/logo.gif",
-    shortcut: "/images/logo.gif",
-    apple: "/images/logo.gif",
+    icon: "/images/logo.png",
+    shortcut: "/images/logo.png",
+    apple: "/images/logo.png",
   },
 };
 
