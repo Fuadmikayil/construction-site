b36d8f8f597e310f26979e2327c70a9211af8668
Fuadmikayil <mikayilov2006@gmail.com>
2025-12-27 22:37:32 +0400

    ABOUT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 import Services from "../components/Services";
 import ProductsSlider from "../components/ProductsSlider";
 import PartnersSection from "../components/PartnersSection";
-
+////Developer: Fuad Mikayilov  instagram: @By_F37D  whatsapp: +994555407687
 export default function Home() {
   return (
     <main  >
